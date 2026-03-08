3dced2c61d26e6935fe31d7efc0b227a0bc1f981
Venkat Sai GS <gsvenkatsai@gmail.com>
2026-03-08 22:43:39 +0530

    Fetch Platfroms with query client

--- src/components/PlatformSelecter.tsx
+++ src/components/PlatformSelecter.tsx
@@ -16,7 +16,7 @@ const PlatformSelecter = ({ onSelectPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
