dc6e6e5a04128b68d72647a7ad42c74bfbbaa346
Venkat Sai GS <gsvenkatsai@gmail.com>
2026-03-10 23:23:18 +0530

    Handling Errors

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,18 @@
+import { Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <Heading>Oops..</Heading>
+      <Text>
+        {isRouteErrorResponse(error)
+          ? "This Page doesnot exist"
+          : "An unexpected error occured"}
+      </Text>
+    </>
+  );
+};
+
+export default ErrorPage;
