307df4a10c3c29624675f07dcec0434b1a4726ae
Venkat Sai GS <gsvenkatsai@gmail.com>
2026-03-11 22:59:42 +0530

    Building Game ScreenShots component with responsive layout

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,20 @@
+import useScreenshots from "../hooks/useScreenshots";
+import { Image } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useScreenshots(gameId);
+  if (isLoading) return null;
+  if (error) throw error;
+  return (
+    <div className="grid grid-cols-1 min-[450px]:grid-cols-[3fr_3fr] gap-2 grid-rows-[auto] h-screen">
+      {data?.results.map((file) => (
+        <Image key={file.id} src={file.image}></Image>
+      ))}
+    </div>
+  );
+};
+
+export default GameScreenshots;
